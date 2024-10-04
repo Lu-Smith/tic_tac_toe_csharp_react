9d6b90364b26ed78547fdce8528fbbad603e3d93
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-04 16:24:37 +0100

    Updated TicTacToe.cs

--- api/Models/TicTacToe.cs
+++ api/Models/TicTacToe.cs
@@ -7,6 +7,18 @@ namespace api.Models
 {
     public class TicTacToe
     {
-        
+        public char[] Board { get; set; }
+        public char CurrentPlayer { get; set; }
+        public bool IsGameOver { get; set; }
+        public string Result { get; set; }
+
+        // initial board
+        public TicTacToe()
+        {
+            Board = new char[9] { '-', '-', '-', '-', '-', '-', '-', '-', '-' };
+            CurrentPlayer = 'X';
+            IsGameOver = false;
+            Result = "Game in progress";
+        }
     }
 }
