47624cd4f818b31a78372d4d7fa6488e085628e8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-04 16:41:56 +0100

    Created GameControllers.cs

---
+++ api/Controllers/GameControllers.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameControllers : ControllerBase
+    {
+        private static TicTacToe _game = new TicTacToe();
+
+        [HttpGet("board")]
+        public IActionResult GetBoard()
+        {
+            return Ok(new
+            {
+                Board = _game.Board,
+                CurrentPlayer = _game.CurrentPlayer,
+                IsGameOver = _game.IsGameOver,
+                Result = _game.Result
+            });
+        }
+
+        [HttpPost("move")]
+        public IActionResult MakeMove([FromQuery] int index)
+        {
+            if (_game.IsGameOver)
+                return BadRequest("The game is already over!");
+
+            var result = _game.MakeMove(index);
+            return Ok(new
+            {
+                Board = _game.Board,
+                CurrentPlayer = _game.CurrentPlayer,
+                IsGameOver = _game.IsGameOver,
+                Result = result
+            });
+        }
+    }
+}
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c60baefc0d920ee0062dfd9cb3a5f54bddf7777")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248758ced8621a4daf837a1fe8899bdf97264e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
