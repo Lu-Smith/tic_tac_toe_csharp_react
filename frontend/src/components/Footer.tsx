8d44843d0d24b4863575775e699598d6959b20ea
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-05 17:30:41 +0100

    Created Footer.tsx

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+        Coded by
+        <a href='https://www.lunasmithart.com' target='_blank' role='portfolio'>Luna Smith</a>
+        , open-source on
+        <a href='https://github.com/Lu-Smith/tic_tac_toe_csharp_react' target='_blank' role='GitHub'>GitHub</a>.
+    </footer>
+  )
+}
+
+export default Footer
